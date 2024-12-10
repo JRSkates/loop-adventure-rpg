0a85d5e2e568b86adabd3d6e9d5377f36b79c198
JRSkates <skatesproduction@gmail.com>
2024-12-10 14:34:47 +0000

    Enemy class created

---
+++ src/Enemy.h
@@ -0,0 +1,29 @@
+#ifndef _ENEMY_H_
+#define _ENEMY_H_
+
+#include <string>
+#include "Player.h"
+
+class Enemy {
+private:
+    std::string name;
+    int health;
+    int attack_power;
+
+public:
+    // Constructor
+    Enemy(std::string name, int health, int attack_power);
+
+    // Getters
+    std::string get_name() const;
+    int get_health() const;
+    int get_attack_power() const;
+
+    // Methods
+    void take_damage(int damage);
+    void attack(Player& player) const;
+
+    bool is_alive() const;
+};
+
+#endif // _ENEMY_H_
